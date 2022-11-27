e3a421530eda701ad803af35675d34b40839b369
Jacek Kosinski <kashiash@gmail.com>
2022-11-27 11:32:24 +0100

    Initial Commit

---
+++ CodableNetworkingh/CodableNetworkinghApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodableNetworkinghApp.swift
+//  CodableNetworkingh
+//
+//  Created by Jacek Kosiński G on 27/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CodableNetworkinghApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
